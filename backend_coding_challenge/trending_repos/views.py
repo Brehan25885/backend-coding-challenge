b50bb69752bb46e2cf228f88a8492964eb5e1761
brehan.mohamed <brehan.mohamed@objects.ws>
2021-12-24 13:26:06 +0200

    refactor code

--- backend_coding_challenge/trending_repos/views.py
+++ backend_coding_challenge/trending_repos/views.py
@@ -3,27 +3,30 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 import requests
 import datetime
+from collections import defaultdict
+
 class LanguageReposView(APIView):
+  
+  
+
     def _prepare_repos_count_dict(self,data):
         """prepare response with dictionary of repos url and repos count for each language"""
-        languages_dict = {}
-        for val in data['items']:
+        d = defaultdict(lambda: defaultdict(list))
+
+        for val in data["items"]:
             if val['language']:
-                if val['language'] in languages_dict:
-                    languages_dict[val['language']]['repos'].append(val['html_url'])
-                    languages_dict[val['language']]['repos_count'] = languages_dict[val['language']]['repos_count']+1
+                d[val['language']]['repo_urls'].append(val['html_url'])
+                if 'repo_count' in d[val['language']]:
+                    d[val['language']]['repo_count']+=1 
                 else:
-                    languages_dict[val['language']]={}
-                    languages_dict[val['language']]['repos']=[val['html_url']]
-                    languages_dict[val['language']]['repos_count']=1
-        return languages_dict
+                    d[val['language']]['repo_count']=1
+        return d
 
 
     def get(self, request):
         """get request method to return response with list of repos urls and number of repos for each language  in the last 30 days"""
         ##repo_url
         URL = "https://api.github.com/search/repositories"
-        ##get last 30 days date
         date_of_last_30_days = (datetime.datetime.now()- datetime.timedelta(30)).date().strftime("%Y-%m-%d")
         #request example https://api.github.com/search/repositories?q=created>2021-06-10&sort=stars&order=desc
         PARAMS = {'q':'created>'+date_of_last_30_days,'sort':'stars','order':'dec'}
